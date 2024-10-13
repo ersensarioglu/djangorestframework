226642618928b283348118424773c939f383d8b4
Ersen Sarioglu <ersen.sarioglu@gmail.com>
2024-10-13 18:59:16 +0100

    serialize model to dict for response data

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,3 +1,4 @@
+from django.forms.models import model_to_dict
 from django.http import JsonResponse
 import json
 from products.models import Product
@@ -6,8 +7,5 @@ def api_home(request, *args, **kwargs):
     model_data = Product.objects.all().order_by("?").first()
     data = {}
     if model_data:
-        data['id'] = model_data.id
-        data['title'] = model_data.title
-        data['content'] = model_data.content
-        data['price'] = model_data.price
+        data = model_to_dict(model_data, fields=['id', 'title', 'price'])
     return JsonResponse(data)
