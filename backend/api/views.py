4c4b4453f4a9832929ee73443cbf192c8ad040d8
Ersen Sarioglu <ersen.sarioglu@gmail.com>
2024-10-13 18:25:16 +0100

    a bit more complex api which echoes params and body

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,4 +1,14 @@
 from django.http import JsonResponse
+import json
 
 def api_home(request, *args, **kwargs):
-    return JsonResponse({"message": "Hi there, this is your Django API response"})
+    body = request.body
+    data = {}
+    try:
+        data = json.loads(body)
+    except:
+        pass
+    data['params'] = dict(request.GET)
+    data['headers'] = dict(request.headers)
+    data['content_type'] = request.content_type
+    return JsonResponse(data)
--- py_client/basic.py
+++ py_client/basic.py
@@ -2,8 +2,8 @@ import requests
 
 endpoint = "http://127.0.0.1:8000/api/"
 
-get_response = requests.get(endpoint)
+get_response = requests.get(endpoint, params={"in":"hi"}, json={"out":"bye"})
 
 print(get_response.text)
-print(get_response.json()['message'])
+print(get_response.json())
 print(get_response.status_code)
