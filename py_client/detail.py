113c2715b85b7eede70bd264430b5f29b6ed8fe3
Ersen Sarioglu <ersen.sarioglu@gmail.com>
2024-10-15 18:09:28 +0100

    retrieve apiview added

---
+++ py_client/detail.py
@@ -0,0 +1,17 @@
+import requests
+
+endpoint = "http://127.0.0.1:8000/api/products/1"
+
+get_response = requests.get(endpoint)
+
+print(get_response.text)
+print(get_response.json())
+print(get_response.status_code)
+
+endpoint = "http://127.0.0.1:8000/api/products/3/"
+
+get_response = requests.get(endpoint)
+
+print(get_response.text)
+print(get_response.json())
+print(get_response.status_code)
