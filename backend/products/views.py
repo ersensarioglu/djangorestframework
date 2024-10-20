8ccce2c724e3d3e46d93f693c69b068357ddaa5f
Ersen Sarioglu <ersen.sarioglu@gmail.com>
2024-10-20 12:09:30 +0100

    Mixin view class added but not used

--- backend/products/views.py
+++ backend/products/views.py
@@ -1,4 +1,4 @@
-from rest_framework import generics
+from rest_framework import generics, mixins
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from django.shortcuts import get_object_or_404
@@ -47,6 +47,40 @@ class ProductDestroyAPIView(generics.DestroyAPIView):
         super().perform_destroy(instance)
 
 product_destroy_view = ProductDestroyAPIView.as_view()
+
+class ProductMixinAPIView(mixins.CreateModelMixin, mixins.ListModelMixin, mixins.RetrieveModelMixin, mixins.DestroyModelMixin, mixins.UpdateModelMixin, generics.GenericAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    lookup_field = 'pk'
+    
+    def get(self, request, *args, **kwargs):
+        pk = kwargs.get("pk")
+        if pk is not None:
+            return self.retrieve(request, *args, **kwargs)
+        return self.list(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
+
+    def delete(self, request, *args, **kwargs):
+        return self.destroy(request, *args, **kwargs)
+
+    def put(self, request, *args, **kwargs):
+        return self.update(request, *args, **kwargs)
+
+    def perform_create(self, serializer):
+        title = serializer.validated_data.get('title')
+        content = serializer.validated_data.get('content') or None
+        if content is None:
+            content = title
+        serializer.save(content=content)
+
+    def perform_update(self, serializer):
+        instance = serializer.save()
+        if not instance.content:
+            instance.content = instance.title
+                
+product_mixin_view = ProductMixinAPIView.as_view()
 
 class ProductListAPIView(generics.ListAPIView):
     '''
--- py_client/update.py
+++ py_client/update.py
@@ -1,6 +1,6 @@
 import requests
 
-endpoint = "http://127.0.0.1:8000/api/products/1/update/"
+endpoint = "http://127.0.0.1:8000/api/products/6/update/"
 
 data = {"title": "Hello world my friend",
         "price": 5}
