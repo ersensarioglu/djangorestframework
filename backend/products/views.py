db35ba282fdc2559477530d9fe46430305fbf007
Ersen Sarioglu <ersen.sarioglu@gmail.com>
2024-10-20 11:14:07 +0100

    alternative function based views instead of generic views

--- backend/products/views.py
+++ backend/products/views.py
@@ -1,4 +1,7 @@
 from rest_framework import generics
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from django.shortcuts import get_object_or_404
 
 from .models import Product
 from .serializers import ProductSerializer
@@ -31,3 +34,27 @@ class ProductListAPIView(generics.ListAPIView):
     serializer_class = ProductSerializer
 
 product_list_view = ProductListAPIView.as_view()
+
+@api_view(['GET', 'POST'])
+def product_alt_view(request, pk=None, *args, **kwargs):
+    method = request.method
+    
+    if method == "GET":
+        if pk is not None:
+            obj = get_object_or_404(Product, pk=pk)
+            data = ProductSerializer(obj, many=False).data
+        else:   
+            queryset = Product.objects.all()
+            data = ProductSerializer(queryset, many=True).data
+        return Response(data)
+    if method == "POST":
+        serializer = ProductSerializer(data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            title = serializer.validated_data.get('title')
+            content = serializer.validated_data.get('content') or None
+            if content is None:
+                content = title
+            serializer.save(content=content)
+            return Response(serializer.data)
+        return Response({"invalid": "not good data"}, status=400)
+        
