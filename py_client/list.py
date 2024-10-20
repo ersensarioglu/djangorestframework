3786e9cdd4fa0d377ab2fe37099e7eeae58909db
Ersen Sarioglu <ersen.sarioglu@gmail.com>
2024-10-20 13:16:41 +0100

    Token authentication

--- py_client/list.py
+++ py_client/list.py
@@ -1,10 +1,21 @@
 import requests
+from getpass import getpass
 
-endpoint = "http://127.0.0.1:8000/api/products/"
+auth_endpoint = "http://127.0.0.1:8000/api/auth/"
+password = getpass()
+auth_response = requests.post(auth_endpoint, json={"username": "ersen", "password": password})
+print(auth_response.json())
 
-post_response = requests.get(endpoint)
+if auth_response.status_code == 200:
+    token = auth_response.json()['token']
+    headers = {
+        "Authorization": f"Bearer {token}"
+    }
+    endpoint = "http://127.0.0.1:8000/api/products/"
 
-print(post_response.text)
-print(post_response.json())
-print(post_response.status_code)
+    post_response = requests.get(endpoint, headers=headers)
+
+    print(post_response.text)
+    print(post_response.json())
+    print(post_response.status_code)
 
